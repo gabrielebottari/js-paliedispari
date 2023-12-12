ba378d6d3c3f0020f43fe7093a3e3ef0a78f24cf
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-12 14:49:28 +0100

    END first exercise with style and comment

--- Pali/js/scripts.js
+++ Pali/js/scripts.js
@@ -1,13 +1,33 @@
-function isPalindrome(word) {
-    let reverseWord = word.split('').reverse().join('');
-    return word === reverseWord;
-  }
-  
-  let word = prompt('Inserisci una parola: ');
+/*
+Palidroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
 
-  if (isPalindrome(word)) {
+// prima funzione per capire se la parola è palindroma
+function isPalindrome(word) {
+    // prenderà la parola (stringa) la dividerà in un array di lettere poi invertirà l'ordine
+    // infine le ricompatterà in un'altra parola (stringa).
+    let reverseWord = word.split('').reverse().join('');
+    // se la prima parola e quella inversa corrispondono sarà palindroma
+    return word === reverseWord;
+}
+
+//seconda funzione al click del bottone (vedere onclick su HTML del button)
+function palindromeVerify() {
+    //prende il valore dell'input e in base al risultato della prima funzione
+    let word = document.getElementById('palindrome').value;
+
+    if (isPalindrome(word)) {
     console.log('La parola è palindroma');
+    // Stampa il risultato nell'HTML
+    document.getElementById("result").innerText = 'La parola è palindroma';
+    document.getElementById("result").className = "right";
     
-  } else {
+    } else {
     console.log('La parola non è palindroma');
-  }
+    // Stampa il risultato nell'HTML
+    document.getElementById("result").innerText = 'La parola non è palindroma';
+    document.getElementById("result").className = "wrong";
+    }
+}
