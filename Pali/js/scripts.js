91e8f7972550547c80bb28c44b376ef03735301f
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-12 13:59:24 +0100

    ADD first exercise

--- Pali/js/scripts.js
+++ Pali/js/scripts.js
@@ -1,2 +1,13 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+function isPalindrome(word) {
+    let reverseWord = word.split('').reverse().join('');
+    return word === reverseWord;
+  }
+  
+  let word = prompt('Inserisci una parola: ');
+
+  if (isPalindrome(word)) {
+    console.log('La parola è palindroma');
+    
+  } else {
+    console.log('La parola non è palindroma');
+  }
